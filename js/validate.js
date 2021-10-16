00859d0d21edcbc4a30ba55791ed86d81fe5ecfe
SilasRodrigues19 <silasrodrigues.fatec@gmail.com>
2021-10-15 22:36:46 -0300

    ✨ Added validation on form
    - Improved validation, email pattern and error warning added;
    - Button visible and clickable only if form is valid.

--- js/validate.js
+++ js/validate.js
@@ -1,4 +1,4 @@
-$("#mc-embedded-subscribe-form").validate({
+let isValid = $("#mc-embedded-subscribe-form").validate({
     messages: {
         EMAIL: {
             required: "&bull; Esse campo é obrigatório",
@@ -14,13 +14,32 @@ validate = () => {
     let email = document.getElementById("email").value;
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if (email.match(pattern)) {
-        $(button).css({ 'display': 'block', 'opacity': '1' });
+    if (email.match(pattern) && isValid) {
+        button.style.cssText = "display:block; opacity: 1; transition: opacity ease 2.5s";
     } else {
-        $(button).css({ 'display': 'none', 'opacity': '0' });
+        button.style.cssText = "display:none; opacity: 0; transition: opacity ease 2.5s";
     }
 
 }
+
+email.addEventListener("keydown", (e) => {
+
+    if (e.keyCode === 13) {
+
+        e.preventDefault();
+        Swal.fire({
+            title: 'Erro!',
+            icon: 'error',
+            text: 'Preencha com um e-mail válido e clique no botão Inscrever',
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
+    }
+});
 
 
 button.addEventListener('click', (e) => {
